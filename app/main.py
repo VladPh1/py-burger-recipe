a33f4c869bb49092a98bafeb55935791024e5527
Vladyslav Pihol <freepgvlad@gmail.com>
2025-03-28 19:23:30 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Validator(ABC):
     def __set_name__(self, owner: str, name: str) -> None:
         self.protected_name = "_" + name
 
-    def __get__(self, obj: str, objtype: str = None) -> None:
+    def __get__(self, obj: object, objtype: str = None) -> None:
         return getattr(obj, self.protected_name)
 
     def __set__(self, obj: str, value: int) -> None:
@@ -40,14 +40,14 @@ class OneOf(Validator):
     def __init__(self, options: str) -> None:
         self.options = options
 
-    def validate(self, value: int) -> None:
+    def validate(self, value: str) -> None:
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}.")
 
 
 class BurgerRecipe:
     buns = Number(2, 3)
-    cheese = Number(0, 3)
+    cheese = Number(0, 2)
     tomatoes = Number(0, 3)
     cutlets = Number(1, 3)
     eggs = Number(0, 2)
@@ -68,5 +68,3 @@ class BurgerRecipe:
         self.cutlets = cutlets
         self.eggs = eggs
         self.sauce = sauce
-
-    validator = Validator
